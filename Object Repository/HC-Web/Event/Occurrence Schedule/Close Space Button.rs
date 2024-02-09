0cdc0f5ce2a90c3a35ea8ec21a780f84f9648618
garrettclover <GarrettC@saddleback.com>
2024-02-09 13:23:27 -0800

    Automated Check In and Fixed Serving Op Test

---
+++ Object Repository/HC-Web/Event/Occurrence Schedule/Close Space Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Close Space Button</name>
+   <tag></tag>
+   <elementGuidId>93f0cbc0-25e6-41be-9ba9-fb42f94c374b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@data-testid='room-attendee-details-close-space-btn']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/HC-Web/Event/Occurrence Schedule/Open Room Early Yes Button.rs
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Open Room Early Yes Button</name>
+   <tag></tag>
+   <elementGuidId>91d8abbd-04e6-479f-ac33-59556cd23edc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='check_in_page--open_room_banner_yes_button']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#check_in_page--open_room_banner_yes_button > span.button-inner-container.jss244</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>4ffab873-a95a-4ceb-b0d8-1cd432f50ed8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>button-inner-container jss244</value>
+      <webElementGuid>c21f4000-c7fa-455c-8869-850952424791</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Yes</value>
+      <webElementGuid>0be1924b-e298-463e-bc7e-26c40c78c69b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;check_in_page--open_room_banner_yes_button&quot;)/span[@class=&quot;button-inner-container jss244&quot;]</value>
+      <webElementGuid>63f4d546-8370-46cb-9964-84b49ce5e69e</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
