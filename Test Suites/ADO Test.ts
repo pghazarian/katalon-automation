528b2200de5be1185a95ab1086cb2956e0c95148
crisf <cfrancisco@gmail.com>
2024-02-12 10:19:07 -0800

    Creating test suite for Azure run

---
+++ Test Suites/ADO Test.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>ADO Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3f6e7e24-cbb5-486c-8766-6789d803ce6f</testSuiteGuid>
+   <testCaseLink>
+      <guid>5dccdb99-dbbc-468b-88b8-c81a88a35a95</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/HC-Web/Serving Opps/SO Creation and Sign Up</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
