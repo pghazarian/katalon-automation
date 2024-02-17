903a55a6b62aed04901cedf8172f1b11548b6d17
crisf <cfrancisco@gmail.com>
2024-02-16 17:26:08 -0800

    Handle different widths with the navigation section

---
+++ Object Repository/Saddleback Web/Home/Nav/Menu Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Menu Button</name>
+   <tag></tag>
+   <elementGuidId>32fa14de-ac58-40fe-8397-46462ccf12c9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a[@id='menuBtn']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
--- Scripts/Saddleback Web/Home Page Load and Verification/Script1707946940911.groovy
+++ Scripts/Saddleback Web/Home Page Load and Verification/Script1707946940911.groovy
@@ -14,8 +14,7 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
-
-import graphql.Assert
+import graphql.Assert as Assert
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
@@ -25,24 +24,14 @@ WebUI.openBrowser(GlobalVariable.Saddleback_Welcome_URL)
 'Wait for the Saddleback Logo to be rendered before we verify page content'
 WebUI.waitForElementPresent(findTestObject('Saddleback Web/Home/Saddleback Logo image'), 0)
 
+'Take screenshot of the browser to see what the page looks like'
+WebUI.takeScreenshotAsCheckpoint('Saddleback-Welcome-HomePage')
+
 'Verify the Saddleback Logo is present'
 WebUI.verifyElementPresent(findTestObject('Saddleback Web/Home/Saddleback Logo image'), 0)
 
-'Verify the text for Locations link'
-WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Nav/Location Header link'), 
-    'Locations')
-
-'Verify the text for Watch link'
-WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Nav/Watch Header Link'), 'Watch')
-
-'Verify the text for About link'
-WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Nav/About Header Link'), 'About')
-
-'Verify the text for Care link'
-WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Nav/Care Header Link'), 'Care')
-
-'Verify the text for Give link'
-WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Nav/Give Header Link'), 'Give')
+'Verify the Navigation rendering'
+WebUI.callTestCase(findTestCase('Saddleback Web/Helper/Validate Nav Menu'), [null:null], FailureHandling.STOP_ON_FAILURE)
 
 'Verify the text for Actions links'
 WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Actions/span_New to Saddleback'), 'New to Saddleback?')
@@ -56,11 +45,6 @@ WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Ac
 WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Actions/span_I want to start volunteering'), 
     'I want to start volunteering.')
 
-'Verify the text for Language'
-WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home/Language Label'), 0)
-
-WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Language Label'), 'Language')
-
 'Verify the Watch Now button is present'
 WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home/Watch Now Button'), 0)
 
@@ -68,20 +52,17 @@ WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home
 WebUI.verifyElementText(findTestObject('Object Repository/Saddleback Web/Home/Watch Now Button'), 'Watch Now')
 
 'Verify the text for Latest Message heading is present'
-WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home/Latest Message Heading'), 
-    0)
+WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home/Latest Message Heading'), 0)
 
 'Verify the text for Latest Message is present'
-WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home/Latest Message Title'), 
-    0)
+WebUI.verifyElementPresent(findTestObject('Object Repository/Saddleback Web/Home/Latest Message Title'), 0)
 
 'Verify the text for Latest Message is not blank'
 def messageTitle = WebUI.findWebElement(findTestObject('Object Repository/Saddleback Web/Home/Latest Message Title'), 0)
 
-WebUI.verifyNotEqual(messageTitle.text, "")
+WebUI.verifyNotEqual(messageTitle.text, '')
 
 //WebUI.verifyEqual(messageTitle.text, "Anchored in Peace")
-
 //TO DO: Verify the footer elements
-
 WebUI.closeBrowser()
+
