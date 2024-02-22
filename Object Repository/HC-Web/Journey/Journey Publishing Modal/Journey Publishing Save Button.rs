f436b7bbda7c6c42658cbab739b1356206042a6e
Mark Hamilton <markh@saddleback.com>
2024-02-22 14:44:31 -0800

    Adding the Publishing and Unpublishing of Journey
    Adding the Publishing and Unpublishing of Journey for Open Navigation Journey

---
+++ Object Repository/HC-Web/Journey/Journey Publishing Modal/Journey Publishing Save Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Journey Publishing Save Button</name>
+   <tag></tag>
+   <elementGuidId>331b9dfa-4c36-4a90-8409-dce795216a87</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[(text() =  'Save' or . = 'Save')]/parent::button</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
