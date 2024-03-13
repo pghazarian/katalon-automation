8cfdcaf04890a42b9884ad3dad7905a5e01f8618
garrettclover <GarrettC@saddleback.com>
2024-03-13 13:37:39 -0700

    Automated Service Series Builder
    Automated service series builder test
    
    Added smoke and stage tags to HC-Web tests
    
    Fixed broken smoke tests

---
+++ Object Repository/HC-Web/Mobile Engagement/Service Series Builder/New Service Series Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>New Service Series Button</name>
+   <tag></tag>
+   <elementGuidId>c9fe39d6-17da-4494-8e17-799c4fc02f2f</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='ui-button--service_companion_add_template']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
