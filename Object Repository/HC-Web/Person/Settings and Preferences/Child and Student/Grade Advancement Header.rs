5687a6e11957beff86923f18f8585dc154b49377
crisf <cfrancisco@gmail.com>
2024-03-14 17:51:15 -0700

    Add sub nav checking in the People Nav test

---
+++ Object Repository/HC-Web/Person/Settings and Preferences/Child and Student/Grade Advancement Header.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Grade Advancement Header</name>
+   <tag></tag>
+   <elementGuidId>8e5304e5-26bf-46ce-b68d-035fc614417e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h3[.='Grade Advancement']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
