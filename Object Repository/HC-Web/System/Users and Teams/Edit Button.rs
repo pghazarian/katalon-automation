19bb0dbf543e0b28f8996444f4cef666fed84836
garrettclover <GarrettC@saddleback.com>
2024-03-18 13:27:48 -0700

    Automated Users and Teams Test
    Automated test that verifies elements are rendering in Users and Teams area

---
+++ Object Repository/HC-Web/System/Users and Teams/Edit Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Edit Button</name>
+   <tag></tag>
+   <elementGuidId>21429d02-7685-4c00-99db-e1166239b395</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[text()='Edit']/ancestor::button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
