b86b8811aed30edb6de33736fb2201b6f9a24faa
Mark Hamilton <markh@saddleback.com>
2024-03-19 17:05:47 -0700

    First Attempt at Android Companion App Testing

---
+++ Object Repository/Companion App/Android/Events Navigation Button.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Events Navigation Button</name>
+   <tag></tag>
+   <elementGuidId>fc92674b-c068-4376-bde5-bf7cd5f3604a</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//*[@class = 'android.widget.TextView' and (@text = 'Events' or . = 'Events')]</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
