c2fd0e48b48c66a78619ced4f0e4839e1e4e9e52
garrettclover <GarrettC@saddleback.com>
2024-03-28 15:57:11 -0700

    Migrated Logged Out Android Tests
    Migrated logged out smoke tests for android
    
    Fixed broken element references
    
    Reorganized object structure for android ME tests
    
    Renamed objects for android ME tests

---
+++ Object Repository/ME App Android/More Page/Terms of Service Tile.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Terms of Service Tile</name>
+   <tag></tag>
+   <elementGuidId>2a538d25-e3f2-4bcf-be7d-5fad60f687d8</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//android.view.ViewGroup[@content-desc=&quot;ui-tile--terms&quot;]/android.view.ViewGroup</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
---
+++ Scripts/ME App/Android/Logged Out Tests/Discover/Script1711558165170.groovy
@@ -0,0 +1,47 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('C:\\Users\\gclov\\Downloads\\hc-android-release (5).apk', false)
+
+Mobile.tap(findTestObject('ME App Android/Campus Selector/Continue As Guest Button'), 0)
+
+Mobile.tap(findTestObject('ME App Android/Campus Selector/Campus Selector Anaheim Option'), 0)
+
+Mobile.tap(findTestObject('ME App Android/Campus Selector/Campus Selector Continue Button'), 0)
+
+Mobile.tap(findTestObject('ME App Android/Navigation Items/Discover Tab'), 0)
+
+Mobile.tap(findTestObject('ME App Android/Discover/First Listed Event'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ME App Android/Discover/Events/Register Button'), 0)
+
+Mobile.sendKeys(findTestObject('Object Repository/ME App Android/Discover/Events/First Name Text Field'), 'Test')
+
+Mobile.scrollToText('Email')
+
+Mobile.sendKeys(findTestObject('Object Repository/ME App Android/Discover/Events/Last Name Text Field'), 'User')
+
+Mobile.sendKeys(findTestObject('Object Repository/ME App Android/Discover/Events/Email Text Field'), 'sbchcqa@saddleback.com')
+
+Mobile.tap(findTestObject('Object Repository/ME App Android/Discover/Events/Continue Button'), 0)
+
+Mobile.tap(findTestObject('Object Repository/ME App Android/Discover/Events/Done Button'), 0)
+
+Mobile.closeApplication()
+
