44b1ed0669204e9d6a955616b77a58423edafbb1
Paren Ghazarian <pareng@saddleback.com>
2024-04-01 16:16:40 -0700

    TC for PBIs: 108079 - 108255 - 108073
    PBI 108079: Notifications - Back Office - Task Rule Toggle - Data/Backend
    PBI 108255: Notifications - Back Office - Task Rule Toggle – API
    PBI 108073: Notifications - Back Office - Task Rule Toggle - UI Design

--- Scripts/Ministry App/Notifications - Back Office - Task Rule Toggle - UI Design/Script1712011753351.groovy
+++ Scripts/Ministry App/Notifications - Back Office - Task Rule Toggle - UI Design/Script1712011753351.groovy
@@ -18,14 +18,23 @@ import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
 /*
- * 1. Open HC Admin 
+ * 1. Open HC Admin
  * 2. Login
  * 3. Go to Data Capture > Connection Form
  * 4. Search for an Active Connection Form
- * 5. Open the Active Connection Form 
- * 6. Go to the Follow Ups tab
- * 7. Add a New Follow Up Templete Rule
- * 8. Add a Task Rule to the New Templete Rule
+ * 5. Open the Active Connection Form
+ * 6. Go to the Follow Ups tab of the Connection Form
+ * 7. Create a follow up Templete Rule
+ * 8. Add a Task Rule
  * 9. Go to Notifications section of the Rule Builder part of the Task Rule
- * 10. Verify that only Push Notifications Templates appear in the dropdown menu within the Notifications section 
+ * 10. Select one of each types of following push notifications using the Radio button next to them: None, Daily Batch, Immediate (Must run the TC 3 times, one for each type)
+ * 11  Select the appropriate Notification Templete to be sent for the Notification type selected, from the "Select Notification" drop down menu 	
+ * 12. Enter and Select all other required fields and sections of the Task Rule
+ * 13. Click on the Done button to save the Task Rule
+ * 14. Click on the Save button to save the Rule
+ * 15. Open the Ministry App and Login
+ * 16. Verify that if "None" type is selected NO push notification is received, in your mobile device's Notification Center
+ * 17. Verify that if "Immediate" type is selected a push notification is received instantly, in your mobile device's Notification Center
+ * 18. Verify that if "Daily Batch" type is selected a push notification is received, in your mobile device's Notification Center	
+ * 19. Verify that the "Daily Batch" and "Immediate" types of Notifications are displayed, based on the Notification Templete selected in HC Admin, from the "Select Notification" drop down menu 	
  */
