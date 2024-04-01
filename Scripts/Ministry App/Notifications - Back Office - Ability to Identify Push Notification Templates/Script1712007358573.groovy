504b9d0551af9ca1c3837d9cfe708587488ab823
Paren Ghazarian <pareng@saddleback.com>
2024-04-01 15:09:12 -0700

    TCs for PBIs  108254 - 108071 - 108360
    PBI 108254: Notifications - Back Office - Enable Instant Push Notifications - Engine
    PBI 108071: Notifications - Back Office - Enable Instant Push Notifications – API
    PBI 108360: Notifications - Back Office – Ability to Identify Push Notification Templates

---
+++ Scripts/Ministry App/Notifications - Back Office - Ability to Identify Push Notification Templates/Script1712007358573.groovy
@@ -0,0 +1,31 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+/*
+ * 1. Open HC Admin 
+ * 2. Login
+ * 3. Go to Data Capture > Connection Form
+ * 4. Search for an Active Connection Form
+ * 5. Open the Active Connection Form 
+ * 6. Go to the Follow Ups tab
+ * 7. Add a New Follow Up Templete Rule
+ * 8. Add a Task Rule to the New Templete Rule
+ * 9. Go to Notifications section of the Rule Builder part of the Task Rule
+ * 10. Verify that only push notification templates appear in the dropdown menu within the Notifications section 
+ */
