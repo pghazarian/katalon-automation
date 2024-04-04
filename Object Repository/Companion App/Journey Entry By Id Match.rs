40c1c087bc09ce13ad6f0dac07b3553975adbacd
crisf <cfrancisco@gmail.com>
2024-04-04 09:57:51 -0700

    Add first cut at the Journey Browsing

---
+++ Object Repository/Companion App/Journey Entry By Id Match.rs
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Journey Entry By Id Match</name>
+   <tag></tag>
+   <elementGuidId>e77ee40f-72d1-4bac-9065-27b13cd6581f</elementGuidId>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>**/XCUIElementTypeOther[`name == &quot;card--goto_journey_508&quot;`][4]</locator>
+   <locatorStrategy>IOS_CLASS_CHAIN</locatorStrategy>
+</MobileElementEntity>
--- Scripts/Companion App/Journey/Journey Browsing/Script1712185034408.groovy
+++ Scripts/Companion App/Journey/Journey Browsing/Script1712185034408.groovy
@@ -25,6 +25,8 @@ import com.detroitlabs.katalonmobileutil.touch.Swipe as Swipe
 import com.detroitlabs.katalonmobileutil.touch.Swipe.SwipeDirection as SwipeDirection
 
 def timeout = 3
+def journeyId = 508
+def allowJourneyWalkthrough = false
 
 'Open existing app by the app bundle id'
 WebUI.callTestCase(findTestCase('Companion App/Shared/Login'), [('UserName') : GlobalVariable.Admin2_UserName, ('Password') : GlobalVariable.Admin2_Password], 
@@ -36,25 +38,88 @@ Button.tap('Nav/Journey Navigation Button', timeout)
 'Verify that the Header is present'
 Mobile.verifyElementVisible(Finder.findLabel('Journey Heading'), timeout)
 
-'Click Link for the Journey Walkthrough'
-Button.tap(Finder.findLink('What is a Journey Link'), timeout)
+if (allowJourneyWalkthrough) {
+	'Click Link for the Journey Walkthrough'
+	Button.tap(Finder.findLink('What is a Journey Link'), timeout)
+	
+	'Do the walkthrough of the Journey'
+	
+	'Verify text on page 1'
+	Mobile.verifyElementVisible(Finder.findLabel('Walkthough Page 1 Heading'), timeout)
+	
+	'Swipe'
+	Swipe.swipe(SwipeDirection.RIGHT_TO_LEFT)
+	
+	'Verify text on page 2'
+	Mobile.verifyElementVisible(Finder.findLabel('Walkthough Page 2 Heading'), timeout)
+	
+	'Swipe'
+	Swipe.swipe(SwipeDirection.RIGHT_TO_LEFT)
+	
+	'Verify text on page 3'
+	Mobile.verifyElementVisible(Finder.findLabel('Walkthough Page 3 Heading'), timeout)
+	
+	'Click Browse Journeys Button'
+	Button.tap('Journey/Browse Journeys')
+}
 
-'Do the walkthrough of the Journey'
+'Verify that there are search results'
 
-'Verify text on page 1'
-Mobile.verifyElementVisible(Finder.findLabel('Walkthough Page 1 Heading'), timeout)
+'Click on the Journey record that we are finding'
+Mobile.tap(findTestObject('Object Repository/Companion App/Journey Entry By Id Match'), timeout)
+
+Mobile.verifyElementVisible(Finder.findLabel('Journey/Details/Description'), timeout)
+
+Mobile.verifyElementVisible(Finder.findLabel('Journey/Details/Heading'), timeout)
+
+Mobile.verifyElementVisible(Finder.findLabel('Journey/Details/Subtitle'), timeout)
 
 'Swipe'
-Swipe.swipe(SwipeDirection.RIGHT_TO_LEFT)
+Swipe.swipe(SwipeDirection.BOTTOM_TO_TOP)
 
-'Verify text on page 2'
-Mobile.verifyElementVisible(Finder.findLabel('Walkthough Page 2 Heading'), timeout)
+'Start Journey'
+Mobile.verifyElementVisible(Finder.findButton('Journey/Details/Start Journey'), timeout)
 
-'Swipe'
-Swipe.swipe(SwipeDirection.RIGHT_TO_LEFT)
+Mobile.tap(Finder.findButton('Journey/Details/Start Journey'), timeout)
 
-'Verify text on page 3'
-Mobile.verifyElementVisible(Finder.findLabel('Walkthough Page 3 Heading'), timeout)
+Mobile.verifyElementVisible(Finder.findLabel('Journey/Details/Journey In Progress Status'), timeout)
 
-Button.tap('Journey/Browse Journeys')
+Mobile.verifyElementVisible(Finder.findButton('Journey/Details/See Journey Pathway'), timeout)
+
+Mobile.verifyElementVisible(Finder.findButton('Journey/Details/Stop Pathway'), timeout)
+
+'Stop Journey with No Prompt'
+Mobile.tap(Finder.findButton('Journey/Details/Stop Journey'), timeout)
+
+Mobile.tap(Finder.findButton('Journey/Details/Prompt Stop Journey No'), timeout)
+
+Mobile.verifyElementVisible(Finder.findLabel('Journey/Details/Journey In Progress Status'), timeout)
+
+'Stop Journey with Yes Prompt'
+Mobile.tap(Finder.findButton('Journey/Details/Stop Journey'), timeout)
+
+Mobile.tap(Finder.findButton('Journey/Details/Prompt Stop Journey Yes'), timeout)
+
+'See the Popup'
+Mobile.verifyElementVisible(Finder.findLabel('Popup/Body'), timeout)
+
+Mobile.verifyElementVisible(Finder.findLabel('Popup/Title'), timeout)
+
+'Close the Popup'
+Mobile.tap(Finder.findButton('Popup/Close'), timeout)
+
+'Verify the Journey is not in progress'
+Mobile.verifyElementNotVisible(Finder.findLabel('Journey/Details/Journey In Progress Status'), timeout)
+
+'Close the Journey'
+Mobile.tap(Finder.findButton('Journey/Details/Journey Close Top'), timeout)
+
+
+
+
+
+
+
+ 
+
 
