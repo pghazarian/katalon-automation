0c4be0a207409737732e5776acad9daef0ab5cd9
garrettclover <GarrettC@saddleback.com>
2024-04-05 10:37:05 -0700

    Automated Navigate to Person Record through Check In

---
+++ Object Repository/HC-Web/Event/Check In/Person Search Drawer/Edit Person Record Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Edit Person Record Button</name>
+   <tag></tag>
+   <elementGuidId>dfeb0bf7-6a85-4b2d-b747-7028d72aed10</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='person_record--edit_person']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
