22dcf1355602d701187af7fe88b4149fb6361a93
garrettclover <GarrettC@saddleback.com>
2024-04-08 12:22:32 -0700

    Automated Test for Creating Person Record via Check In

---
+++ Object Repository/HC-Web/Event/Check In/Add Attendee Drawer/New Person Drawer/Child Birthdate Text Field.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Child Birthdate Text Field</name>
+   <tag></tag>
+   <elementGuidId>fb5f5491-f15c-4b58-864a-aac24c5077df</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='new_attendee_with_child--child_birthdate']/descendant::input</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
