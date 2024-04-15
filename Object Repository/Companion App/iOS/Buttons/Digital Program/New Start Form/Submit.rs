63bbf5f48ce6e0fe4a5e9a7b998d5e8d7081e8f1
crisf <cfrancisco@gmail.com>
2024-04-15 16:59:50 -0700

    Add first cut of these new tests

---
+++ Object Repository/Companion App/iOS/Buttons/Digital Program/New Start Form/Submit.rs
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>Submit</name>
+   <tag></tag>
+   <elementGuidId>d10ca115-9c3c-4677-b2e9-f90c0177a696</elementGuidId>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>Submit</locator>
+   <locatorStrategy>ACCESSIBILITY</locatorStrategy>
+</MobileElementEntity>
---
+++ Object Repository/Companion App/iOS/Text Fields/Digital Program/New Start Form/First Name.rs
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>First Name</name>
+   <tag></tag>
+   <elementGuidId>fcae90df-3305-4ac5-a501-faca38830503</elementGuidId>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//XCUIElementTypeStaticText[@name=&quot;First Name&quot;]/../following-sibling::XCUIElementTypeTextField[1]</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
