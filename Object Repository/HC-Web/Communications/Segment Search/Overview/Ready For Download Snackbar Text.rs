3b10d5da372f2f071e0f62daab1ce8ad5e7f231f
sanchita-tiwari <34640678+sanchita-tiwari@users.noreply.github.com>
2024-04-15 16:46:21 +0545

    Created Export Segment Record Results
    Added a test case to export segment record results in the CSV and XLSX formats

---
+++ Object Repository/HC-Web/Communications/Segment Search/Overview/Ready For Download Snackbar Text.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Ready For Download Snackbar Text</name>
+   <tag></tag>
+   <elementGuidId>0b3bc7c4-5d1b-4fe7-bce1-58302c65714a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='MuiSnackbarContent-message jss611']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/HC-Web/Communications/Segment Search/Overview/Ready For Download Toast Text.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Ready For Download Toast Text</name>
+   <tag></tag>
+   <elementGuidId>36fee370-56ee-45af-b9d9-95b0f20c040a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//span[@class='font-size-xxsmall']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
