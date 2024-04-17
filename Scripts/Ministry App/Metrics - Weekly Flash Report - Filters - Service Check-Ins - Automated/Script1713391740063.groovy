16f5f62035a2f680b4269bea554401ddbcf3bd0d
Paren Ghazarian <pareng@saddleback.com>
2024-04-17 15:38:57 -0700

    Start of automation of 2 TCs for PBI 108337
    Product Backlog Item 108337: Metrics - Weekly Flash Report - Filters UI for Service Check-Ins/CTAs

--- Scripts/Ministry App/Metrics - Weekly Flash Report - Filters - Service Check-Ins - Automated/Script1713391740063.groovy
+++ Scripts/Ministry App/Metrics - Weekly Flash Report - Filters - Service Check-Ins - Automated/Script1713391740063.groovy
@@ -16,33 +16,15 @@ import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
-import io.appium.java_client.TouchAction as TouchAction
-import io.appium.java_client.touch.offset.PointOption as PointOption
-import io.appium.java_client.touch.WaitOptions as WaitOptions
-import java.time.Duration as Duration
-import io.appium.java_client.android.AndroidDriver as AndroidDriver
-import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory as MobileDriverFactory
-import io.appium.java_client.AppiumDriver as AppiumDriver
 
-WebUI.callTestCase(findTestCase('Ministry App/Login'), [:], FailureHandling.STOP_ON_FAILURE)
+WebUI.callTestCase(findTestCase('Ministry App/Login'), [('UserName') : 'Pareng@Saddleback.com', ('Password') : 'bGzvpoZCcaztWWDnaA3/tA=='
+        , ('ForceLogin') : false], FailureHandling.STOP_ON_FAILURE)
 
 Mobile.tap(findTestObject('Ministry App/Navigation/Metrics icon'), 0)
 
 'To open Trending page'
 Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Trending tab'), 0)
 
-Mobile.tap(findTestObject('Ministry App/Metrics/Service Check-Ins tile'), 0)
-
-'To open 8 Weeks trends for Service Check-Ins'
-Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/8 Week Trend button'), 0)
-
-'This will open the actual weekly trend chart\'s pop up modal for Service Check-Ins & CTAs for the Latest Week'
-Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Chart for the latest week'), 0)
-
-'To close the pop up '
-Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/X button'), 0)
-
-Mobile.scrollToText('Month')
-
-Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Month button'), 0)
+'To open  Service Check-Ins Trending Reports'
+Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Service Check-Ins tile'), 0)
 
