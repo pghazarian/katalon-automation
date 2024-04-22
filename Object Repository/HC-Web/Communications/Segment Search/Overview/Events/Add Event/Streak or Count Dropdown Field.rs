b0631729f8261ce829d3cdc7ab23af3b35318603
sanchita-tiwari <34640678+sanchita-tiwari@users.noreply.github.com>
2024-04-22 20:43:44 +0545

    Added new test cases for Events
    Added new test cases for Events, and updated Export Segment Record Results

---
+++ Object Repository/HC-Web/Communications/Segment Search/Overview/Events/Add Event/Streak or Count Dropdown Field.rs
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Streak or Count Dropdown Field</name>
+   <tag></tag>
+   <elementGuidId>51648642-a05a-40d2-b5a9-2cd7203d254d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@data-testid='event-type-selector']/descendant::div[@class='Select-control']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Scripts/HC-Web/Communications/Segment Search/Overview/Add Events By Event and Streak/Script1713346897020.groovy
@@ -0,0 +1,69 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+not_run: WebUI.callTestCase(findTestCase('HC-Web/Communications/Segment Search/Shared/Create Segment'), [:], FailureHandling.STOP_ON_FAILURE)
+
+'Login'
+WebUI.callTestCase(findTestCase('HC-Web/Shared/Login'), [('HostUrl') : GlobalVariable.HC_HostUrl, ('UserName') : GlobalVariable.Admin_UserName
+        , ('Password') : GlobalVariable.Admin_Password, ('TargetPath') : '/communications-central/segment-search'], FailureHandling.STOP_ON_FAILURE)
+
+SearchTerm = 'ST Segment For Event Test 4'
+
+'Type Search term'
+WebUI.setText(findTestObject('Object Repository/HC-Web/Communications/Segment Search/Search Text Field'), SearchTerm)
+
+'Initiate Search'
+WebUI.sendKeys(findTestObject('Object Repository/HC-Web/Communications/Segment Search/Search Text Field'), Keys.chord(Keys.ENTER))
+
+'Open the segment record from the search results'
+WebUI.click(findTestObject('HC-Web/Communications/Segment Search/Table Row', [('SegmentName') : SearchTerm]))
+
+'Click on the Edit button next to Events'
+WebUI.click(findTestObject('HC-Web/Communications/Segment Search/Overview/Events/Events Edit Button'))
+
+'Scroll to Events header'
+WebUI.scrollToElement(findTestObject('HC-Web/Communications/Segment Search/Overview/Events/Events Header'), 0)
+
+'Click button to select Event'
+WebUI.click(findTestObject('HC-Web/Communications/Segment Search/Overview/Events/Add Event/Event Type Radio Button', [('index') : 1]))
+
+'Select event from dropdown'
+CustomKeywords.'TestObjectHelper.setDropDownValueByXPath'('//div[@name=\'event-selector\']/descendant::div[@class=\'Select-control\']', 
+    'ST Event 1')
+
+'Select first recent sequence from dropdown'
+CustomKeywords.'TestObjectHelper.setDropDownValueByXPath'('//div[@name=\'event-type-selector\']/div[\'name=select-menu-scrollbar\']/descendant::div[@class=\'Select-option\']', 
+    'Attended')
+
+'Select count from dropdown'
+not_run: CustomKeywords.'TestObjectHelper.setDropDownValueByXPath'('//div[@data-testid=\'event-first-attendance-selector\']/descendant::div[@class=\'Select-control\']', 
+    '1')
+
+'Select second recent sequence from dropdown'
+not_run: CustomKeywords.'TestObjectHelper.setDropDownValueByXPath'('//div[@data-testid=\'event-first-attendance-selector\']/descendant::div[@class=\'Select-control\']', 
+    'Unattended')
+
+'Select count from dropdown'
+not_run: CustomKeywords.'TestObjectHelper.setDropDownValueByXPath'('//div[@data-testid=\'event-first-attendance-selector\']/descendant::div[@class=\'Select-control\']', 
+    '1')
+
+not_run: WebUI.click(findTestObject('Object Repository/HC-Web/Communications/Segment Search/Overview/Events/Add Event/Event Save Button'))
+
+not_run: WebUI.closeBrowser()
+
