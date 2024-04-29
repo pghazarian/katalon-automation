8d602e980faa34e7d2502185c43175fdd19b1677
Paren Ghazarian <pareng@saddleback.com>
2024-04-28 21:00:18 -0700

    Partial automated TC for PBI 109145
    Product Backlog Item 109145: Metrics - Trend Reports - Filter by Campus Permissions

--- Scripts/Ministry App/Metrics/Verify Filter View by Campus Permissions - Part. Auto/Script1714242592184.groovy
+++ Scripts/Ministry App/Metrics/Verify Filter View by Campus Permissions - Part. Auto/Script1714242592184.groovy
@@ -17,20 +17,23 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
-/*
- * Preprquisite: When user has Permissions on HC Admin side for Specific Campuses locations, they should only see Data for those Campuses in the Trend Reports on MA 
- * 1. Open Ministry App
- * 2. Login
- * 3. Tap on Metrics icon
- * 4. Tap on Trending tab
- * 5. Tap on Attendence tile
- * 6. Tap on Filter icon
- * 7. Verify Filters drawer pops open from the bottom of the screen
- * 8. Verify Campus field exists with a ">" Chevron icon to the far right of it
- * 9. Select the Campus or Campuses which the user has permissions to view on HC Admin
- * 10. Tap on the Search button 
- * 11. Verify that user can only view Attendence data for Campuses that they have Permission on in HC Admin 
- * 12. Parameterization: Repeat step 5, for each one of the following trend reports tiles: Baptisms, Decisons, Service Check-Ins, & CTAs
- * 13. Verify that user can only view the data counts for Baptisms, Decisons, Service Check-Ins, & CTAs for the campuses that they have permisson to view on HC admin 
- * 14. Repeat the same steps from the "Weekly Flash" tab's drop down menu, for the following trend reports types: Attendence, Baptisms, Decisons, Service Check-Ins, & CTAs
- */
+WebUI.callTestCase(findTestCase('Ministry App/Shared TCs/Login'), [('UserName') : 'Pareng@Saddleback.com', ('Password') : 'bGzvpoZCcaztWWDnaA3/tA=='
+        , ('ForceLogin') : false], FailureHandling.STOP_ON_FAILURE)
+
+Mobile.tap(findTestObject('Ministry App/Navigation/Metrics icon'), 0)
+
+'To open Trending page'
+Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Trending tab'), 0)
+
+'To open Attendance menu options of Trending Reports '
+Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Attendance tile'), 0)
+
+'To choose "Attendance Trend Report" menu option '
+Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Attendance Trend Report - menu option'), 0)
+
+'To open the Filtering page for the Attendance trend reports  '
+Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Filter icon'), 0)
+
+'To see all available Campus(es) - based on the logged in user\'s Permissions in HC admin '
+Mobile.tap(findTestObject('Object Repository/Ministry App/Metrics/Campus - chevron icon'), 0)
+
