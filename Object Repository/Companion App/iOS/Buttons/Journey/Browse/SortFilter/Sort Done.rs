95297355d3f4c33b8308fc09e58f5ff5466a3e2a
markhsbc <markh@saddleback.com>
2024-05-01 15:39:57 -0700

    Journey Browse - final iOS

--- Object Repository/Companion App/iOS/Buttons/Journey/Browse/SortFilter/Sort Done.rs
+++ Object Repository/Companion App/iOS/Buttons/Journey/Browse/SortFilter/Sort Done.rs
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <MobileElementEntity>
    <description></description>
-   <name>Back</name>
+   <name>Sort Done</name>
    <tag></tag>
-   <elementGuidId>2ae1541e-c407-4058-9bf0-f6490158f7d7</elementGuidId>
+   <elementGuidId>3be24131-0d25-48b9-8850-3834cb831dec</elementGuidId>
    <selectorMethod>BASIC</selectorMethod>
    <useRalativeImagePath>false</useRalativeImagePath>
-   <locator>link</locator>
+   <locator>done_button</locator>
    <locatorStrategy>ACCESSIBILITY</locatorStrategy>
 </MobileElementEntity>
