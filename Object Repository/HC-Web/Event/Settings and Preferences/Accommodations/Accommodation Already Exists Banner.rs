92104349e5b01f01d62530547b2f8e6ed273f0f0
garrettclover <GarrettC@saddleback.com>
2024-05-02 12:31:50 -0700

    Automated Tags and Accommodations
    Automated two tests for tags and accommodations
    -Verify tags/accommodations can be added and removed
    -Verify duplicate tags/accommodations cannot be added
    
    Fixed SO Creation and Sign Up test and Filter and Sort events test

---
+++ Object Repository/HC-Web/Event/Settings and Preferences/Accommodations/Accommodation Already Exists Banner.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Accommodation Already Exists Banner</name>
+   <tag></tag>
+   <elementGuidId>22654bf2-dcac-475f-aa32-6aeff0770ddb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class='banner-message-container']/p</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/HC-Web/Event/Settings and Preferences/Accommodations/Add Accommodation Button.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Add Accommodation Button</name>
+   <tag></tag>
+   <elementGuidId>a1e01ce0-4c5e-4bc1-8fbc-2e0c86a73bca</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button[@id='ui-button--add_accommodation']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
