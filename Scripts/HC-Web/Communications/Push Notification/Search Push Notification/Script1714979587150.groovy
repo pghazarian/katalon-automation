843e3a3988efb507f4c26691d53f4cfd1e5ef1a8
sanchita-tiwari <34640678+sanchita-tiwari@users.noreply.github.com>
2024-05-06 15:42:30 +0545

    Created Search Push Notification
    Created Search Push Notification

---
+++ Scripts/HC-Web/Communications/Push Notification/Search Push Notification/Script1714979587150.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+/*
+ * Log in as Admin or a non-admin user that has permission to "Manage Push Notification"
+ * Click on "Communications Central > Push Notification"
+ * Verify "Notification" column is visible
+ * Verify "Recipients" column is visible
+ * Verify "Campus" column is visible
+ * Verify "Send Date" column is visible
+ * Verify "Status" column is visible
+ * Verify "Application" column is visible
+ * Verify "Created By" column is visible
+ * Type the name of the push notification into the Search bar and hit the Enter key
+ * Verify the push notification is visible
+ * Click on the push notification
+ * Verify user is redirected to the Detail page
+ */
