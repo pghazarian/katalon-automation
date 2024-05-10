1628fc1f3a29ba4484a9389353b2d6b658b82b85
garrettclover <GarrettC@saddleback.com>
2024-05-10 13:54:08 -0700

    Automated Custom Location Serving Opp Test
    Automated a test that creates a new off-campus venue for an SO and signs up a user, then closes the SO

---
+++ Object Repository/HC-Web/Serving Opps/Off Campus Venue Creation Drawer/Name Text Field.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Name Text Field</name>
+   <tag></tag>
+   <elementGuidId>069020b2-f662-4aab-8e20-b92cc253cb8b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='off_campus_space_drawer--name']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/HC-Web/Serving Opps/Off Campus Venue Creation Drawer/Zip Code Text Field.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Zip Code Text Field</name>
+   <tag></tag>
+   <elementGuidId>297420da-e5c6-4881-b781-c4bfeeeee2aa</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//input[@id='off_campus_space_drawer--postal_code']</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
