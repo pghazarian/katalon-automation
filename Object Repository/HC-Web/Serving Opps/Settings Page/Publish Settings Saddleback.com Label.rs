64c57e09f4e5684413078c1f878c405ad3c43fcd
garrettclover <GarrettC@saddleback.com>
2024-06-11 13:55:40 -0700

    Automated Publish SO Tests
    Automated two tests around SO publishing
    -Publish/Unpublish Public SO
    -Publish/Unpublish Private SO

---
+++ Object Repository/HC-Web/Serving Opps/Settings Page/Publish Settings Saddleback.com Label.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Publish Settings Saddleback.com Label</name>
+   <tag></tag>
+   <elementGuidId>78cff09e-e5f2-4e01-ad3c-1b38675ea67e</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//h4[contains(text(),'Saddleback.com')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
